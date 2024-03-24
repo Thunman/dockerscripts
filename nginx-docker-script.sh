c2860f252dca80f73a1b9b72cdfca50f59bbf763
Thunman <thunman42@gmail.com>
2024-03-24 13:57:47 +0000

    bug fixes

--- nginx-docker-script.sh
+++ nginx-docker-script.sh
@@ -3,7 +3,7 @@ docker run -d \
 --network my-net \
 --restart=unless-stopped \
 -p 443:443 \
--v /etc/letsencrypt/live/thunman.servebeer.com:/etc/nginx/ssl:ro \
+-v ~/ssl:/etc/nginx/ssl:ro \
 -v /var/run/docker.sock:/tmp/docker.sock:ro \
--v /config-files/nginx/nginx-server.conf:/etc/nginx/nginx.conf:ro \
+-v ~/config-files/nginx/nginx-server.conf:/etc/nginx/nginx.conf:ro \
 nginx
