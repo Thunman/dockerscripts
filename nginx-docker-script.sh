ab24cf99ab2f4253e4119333ac01e1ed9216b72e
Thunman <thunman42@gmail.com>
2024-03-27 16:40:25 +0000

    fix

--- nginx-docker-script.sh
+++ nginx-docker-script.sh
@@ -3,7 +3,7 @@ docker run -d \
 --network my-net \
 --restart=unless-stopped \
 -p 443:443 \
--v ~/ssl:/etc/nginx/ssl:ro \
+-v ~/.ssl:/etc/nginx/ssl:ro \
 -v /var/run/docker.sock:/tmp/docker.sock:ro \
--v ~/config-files/nginx/nginx-server.conf:/etc/nginx/nginx.conf:ro \
+-v ~/config-files/nginx.conf:/etc/nginx/nginx.conf:ro \
 nginx
